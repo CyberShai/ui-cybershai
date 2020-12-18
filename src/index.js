5fc5334e060c59b248149feb67b31bfd7fcd01fa
Héctor Reyes <h.reyes@idventiva.com>
2020-12-18 07:37:43 -0600

    NEW: Styles Config

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from "react";
 import { render } from "react-dom";
 
 import App from "./routes/App";
+import "./assets/styles/Main.scss";
 
 render(<App />, document.getElementById("root"));
