49a585974413646dcf29296b2c1532afd26ee358
Héctor Reyes <h.reyes@idventiva.com>
2020-12-18 12:48:33 -0600

    NEW: Component IconText

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,31 +1,12 @@
 import React from "react";
 
-import { Icon } from "../atomic/atoms/Icons/Icon"
+import { IconText } from "../atomic/atoms/IconText/IconText"
 
 const App = () => {
   return <>
-  <h3>Prueba de Iconos</h3>
-  <Icon svg="linkedin" classes="svg-icon" title="linkedin"/>
-  <Icon svg="notification" classes="svg-icon" title="linkedin"/>
-  <Icon svg="search" classes="svg-icon" title="linkedin"/>
-  <Icon svg="close" classes="svg-icon" title="linkedin"/>
-  <Icon svg="bookmark" classes="svg-icon" title="linkedin"/>
-  <Icon svg="chevron" classes="svg-icon" title="linkedin"/>
-  <Icon svg="discord" classes="svg-icon" title="linkedin"/>
-  <Icon svg="github" classes="svg-icon" title="linkedin"/>
-  <Icon svg="user" classes="svg-icon" title="linkedin"/>
-  <Icon svg="chart" classes="svg-icon" title="linkedin"/>
-  <Icon svg="envelope" classes="svg-icon" title="linkedin"/>
-  <Icon svg="check" classes="svg-icon" title="linkedin"/>
-    <Icon svg="suitcase" classes="svg-icon" title="linkedin"/>
-  <Icon svg="danger" classes="svg-icon" title="linkedin"/>
-  <Icon svg="warning" classes="svg-icon" title="linkedin"/>
-  <Icon svg="logout" classes="svg-icon" title="linkedin"/>
-  <Icon svg="user" classes="svg-icon" title="linkedin"/>
-  <Icon svg="trash" classes="svg-icon" title="linkedin"/>
-    <Icon svg="location" classes="svg-icon" title="linkedin"/>
-    <Icon svg="taco" classes="svg-icon" title="linkedin"/>
-    <Icon svg="info" classes="svg-icon" title="linkedin"/>
+  <h3>Atomos Héctor</h3>
+  <IconText icon="github" text="Icono de Menú" link="#" color/>
+  <IconText icon="github" text="Icono de Menú" link="#" />
   </>
 };
 
