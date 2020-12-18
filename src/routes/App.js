2d9ac27a23329695cdb05663d28f8687c16680f6
Djkde <sergio.estrella@utp.edu.co>
2020-12-18 17:19:21 -0500

    NEW: Component TextArea

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,32 +1,34 @@
 import React from "react";
 
-import { Icon } from "../atomic/atoms/Icons/Icon"
+import { Icon } from "../atomic/atoms/Icons/Icon";
 
 const App = () => {
-  return <>
-  <h3>Prueba de Iconos</h3>
-  <Icon svg="linkedin" classes="svg-icon" title="linkedin"/>
-  <Icon svg="notification" classes="svg-icon" title="linkedin"/>
-  <Icon svg="search" classes="svg-icon" title="linkedin"/>
-  <Icon svg="close" classes="svg-icon" title="linkedin"/>
-  <Icon svg="bookmark" classes="svg-icon" title="linkedin"/>
-  <Icon svg="chevron" classes="svg-icon" title="linkedin"/>
-  <Icon svg="discord" classes="svg-icon" title="linkedin"/>
-  <Icon svg="github" classes="svg-icon" title="linkedin"/>
-  <Icon svg="user" classes="svg-icon" title="linkedin"/>
-  <Icon svg="chart" classes="svg-icon" title="linkedin"/>
-  <Icon svg="envelope" classes="svg-icon" title="linkedin"/>
-  <Icon svg="check" classes="svg-icon" title="linkedin"/>
-    <Icon svg="suitcase" classes="svg-icon" title="linkedin"/>
-  <Icon svg="danger" classes="svg-icon" title="linkedin"/>
-  <Icon svg="warning" classes="svg-icon" title="linkedin"/>
-  <Icon svg="logout" classes="svg-icon" title="linkedin"/>
-  <Icon svg="user" classes="svg-icon" title="linkedin"/>
-  <Icon svg="trash" classes="svg-icon" title="linkedin"/>
-    <Icon svg="location" classes="svg-icon" title="linkedin"/>
-    <Icon svg="taco" classes="svg-icon" title="linkedin"/>
-    <Icon svg="info" classes="svg-icon" title="linkedin"/>
-  </>
+  return (
+    <>
+      <h3>Prueba de Iconos</h3>
+      <Icon svg="linkedin" classes="svg-icon" title="linkedin" />
+      <Icon svg="notification" classes="svg-icon" title="linkedin" />
+      <Icon svg="search" classes="svg-icon" title="linkedin" />
+      <Icon svg="close" classes="svg-icon" title="linkedin" />
+      <Icon svg="bookmark" classes="svg-icon" title="linkedin" />
+      <Icon svg="chevron" classes="svg-icon" title="linkedin" />
+      <Icon svg="discord" classes="svg-icon" title="linkedin" />
+      <Icon svg="github" classes="svg-icon" title="linkedin" />
+      <Icon svg="user" classes="svg-icon" title="linkedin" />
+      <Icon svg="chart" classes="svg-icon" title="linkedin" />
+      <Icon svg="envelope" classes="svg-icon" title="linkedin" />
+      <Icon svg="check" classes="svg-icon" title="linkedin" />
+      <Icon svg="suitcase" classes="svg-icon" title="linkedin" />
+      <Icon svg="danger" classes="svg-icon" title="linkedin" />
+      <Icon svg="warning" classes="svg-icon" title="linkedin" />
+      <Icon svg="logout" classes="svg-icon" title="linkedin" />
+      <Icon svg="user" classes="svg-icon" title="linkedin" />
+      <Icon svg="trash" classes="svg-icon" title="linkedin" />
+      <Icon svg="location" classes="svg-icon" title="linkedin" />
+      <Icon svg="taco" classes="svg-icon" title="linkedin" />
+      <Icon svg="info" classes="svg-icon" title="linkedin" />
+    </>
+  );
 };
 
 export default App;
