4765f411ca92c7532077824959a57dd7640aa9ee
Djkde <sergio.estrella@utp.edu.co>
2020-12-18 22:55:30 -0500

    NEW:Component Search

--- src/routes/App.js
+++ src/routes/App.js
@@ -2,24 +2,14 @@ import React from "react";
 
 import { Hired } from "../atomic/molecules/Hired/Hired";
 import Icon from "../assets/images/Icon.png";
-import { FilterDropdown } from "../atomic/atoms/FilterDropdown/FilterDropdown";
+import { Search } from "../atomic/atoms/Search/Search";
 
 const App = () => {
-  const list = [
-    "Opción 1",
-    "Opción 2",
-    "Opción 3",
-    "Opción 4",
-    "Opción 5",
-    "Opción 6",
-    "Opción 7",
-    "Opción 8",
-  ];
   return (
     <>
       <h3>Atomos Héctor</h3>
       <Hired photo1={Icon} photo2={Icon} photo3={Icon} company="Platzi" />
-      <FilterDropdown options={list} type="large" />
+      <Search />
     </>
   );
 };
