d86e7bc4e4e690686292138bb90683f46e207210
hectordevx <hectordevx@gmail.com>
2020-12-18 20:12:47 -0600

    NEW: Student Pages Setup

---
+++ src/atomic/templates/InformacionPersonal/InformacionPersonal.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const InformacionPersonal = () => {
+  return <h1> Pagina de Información Personal</h1>;
+};
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -2,23 +2,31 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import { VacantesPage } from "../pages/VacantesPage";
+import { EmpresasPage } from "../pages/EmpresasPage";
+import { MisPostulacionesPage } from "../pages/MisPostulacionesPage";
+import { NotFoundPage } from "../pages/NotFoundPage";
+import { InformacionPersonalPage } from "../pages/InformacionPersonalPage";
 
 export const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={VacantesPage} />
-        {/* <Route exact path="/login" component={LoginPage} />
-        <Route exact path="/signup" component={SignUpPage} />
-        <Route exact path="/empresas" component={Empresas} />
-        <Route exact path="/mis-postulaciones" component={MisPostulaciones} />
+        <Route exact path="/empresas" component={EmpresasPage} />
         <Route
           exact
-          path="/informacion'personal"
-          component={InformacionPersonal}
+          path="/mis-postulaciones"
+          component={MisPostulacionesPage}
         />
-        <Route exact path="/hector" component={ComponentesHector} />
-        <Route component={NotFoundPage} /> */}
+        <Route
+          exact
+          path="/informacion-personal"
+          component={InformacionPersonalPage}
+        />
+        <Route component={NotFoundPage} />
+        {/* <Route exact path="/login" component={LoginPage} />
+        <Route exact path="/signup" component={SignUpPage} />
+        */}
       </Switch>
     </BrowserRouter>
   );
