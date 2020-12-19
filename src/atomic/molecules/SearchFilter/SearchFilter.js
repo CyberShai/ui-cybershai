1e0c635c19c07ec6443c70d5589304953e759337
Djkde <sergio.estrella@utp.edu.co>
2020-12-19 09:31:13 -0500

    IMPROVE: SearchFilter props and style

--- src/atomic/molecules/SearchFilter/SearchFilter.js
+++ src/atomic/molecules/SearchFilter/SearchFilter.js
@@ -3,26 +3,37 @@ import { SimpleField } from "../../atoms/SimpleField/SimpleField";
 import { FilterDropdown } from "../../atoms/FilterDropdown/FilterDropdown";
 import { Search } from "../../atoms/Search/Search";
 
-export const SearchFilter = () => {
-  const options = [
-    "Opción",
-    "Opción",
-    "Opción",
-    "Opción",
-    "Opción",
-    "Opción",
-    "Opción",
-  ];
+export const SearchFilter = ({ locationOptions, skillsOptions }) => {
   return (
     <div className="searchfilter">
       <p>Filtros de búsqueda:</p>
       <form className="searchfilter-inputs">
-        <FilterDropdown options={options} />
+        <FilterDropdown options={locationOptions} />
         <SimpleField placeholder="$ Min" size="small" />
         <SimpleField placeholder="$ Max" size="small" />
-        <FilterDropdown options={options} />
+        <FilterDropdown options={skillsOptions} />
         <Search />
       </form>
     </div>
   );
+};
+SearchFilter.defaultProps = {
+  locationOptions: [
+    "Lugar 1",
+    "Lugar 2",
+    "Lugar 3",
+    "Lugar 4",
+    "Lugar 5",
+    "Lugar 6",
+    "Lugar 7",
+  ],
+  skillsOptions: [
+    "Habilidad 1",
+    "Habilidad 2",
+    "Habilidad 3",
+    "Habilidad 4",
+    "Habilidad 5",
+    "Habilidad 6",
+    "Habilidad 7",
+  ],
 };
