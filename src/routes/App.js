cbb1dd25ca61ae1991ec60e3f37641360d352aaa
dfloresdev <hello@dflores.dev>
2020-12-18 20:38:09 -0600

    NEW: Componente TechLabel

--- src/routes/App.js
+++ src/routes/App.js
@@ -2,12 +2,10 @@ import React from "react";
 
 import { Icon } from "../atomic/atoms/Icons/Icon";
 import { Hired } from "../atomic/molecules/Hired/Hired";
-import { UploadFile } from "../atomic/atoms/UploadFile/UploadFile";
 
 const App = () => {
   return (
     <>
-      <UploadFile />
       <h3>Atomos Héctor</h3>
       <Hired photo1={Icon} photo2={Icon} photo3={Icon} company="Platzi" />
     </>
